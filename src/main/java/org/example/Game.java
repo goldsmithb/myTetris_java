cb06dde05a6c363b754974082695d90c543b963d
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2023-02-01 15:15:02 -0500

    add icon

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.net.URL;
 
 public class Game extends JFrame {
     public Game() {
@@ -9,7 +14,13 @@ public class Game extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Component emptyLabel = new Label();
         this.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        // size the frame to fit all content
         this.pack();
+        // set location of window
+        this.setLocationRelativeTo(null);
+        // set an icon image for the window
+        // N.B. - SWING DOES NOT SUPPORT ICONS ON MacOS
+        this.setIconImage(new ImageIcon("icon.png").getImage());
         this.setVisible(true);
     }
     public void processInput() {};

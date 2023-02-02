e3630084023d462a006df12a6947a0d67579314f
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2023-02-02 15:22:34 -0500

    feat: game loop

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -10,17 +10,16 @@ import java.net.URL;
 
 public class Game extends JFrame {
     public enum State { PLAYING, GAMEOVER }
-
+    protected final int FPS = 60;
+    private Arena arena;
     private State state = State.GAMEOVER;
     public Game() {
         initUI();
-        Arena p = new Arena();
-
-        this.add(p);
-        this.setVisible(true);
+        start();
+        play();
     }
 
-    public void initUI() {
+    private void initUI() {
         this.setTitle("myTetris");
         this.setSize(new Dimension (400, 500));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,13 +29,49 @@ public class Game extends JFrame {
         this.setLocationRelativeTo(null);
     }
 
-    public void start() {
+    private void start() {
         setGameState(State.PLAYING);
+        arena = new Arena();
+
+        this.add(arena);
+
+        this.setVisible(true);
+    }
+
+    private void play() {
+        long frameLength = 1000 / FPS;
+        long sleepTime;
+        long nextFrameAt;
+        long current;
+
+        while(state == State.PLAYING) {
+            processInput();
+            update();
+            render();
+
+            current = System.currentTimeMillis();
+            nextFrameAt = current + frameLength;
+            sleepTime = nextFrameAt - System.currentTimeMillis();
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime);
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+
+        }
+
     }
 
     public void setGameState(State s) { state = s; }
     public State getGameState() { return state; }
     public void processInput() {};
     public void update() {};
-    public void render() {};
+    public void render() {
+        arena.repaint();
+    };
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // the constructor will initialize all the things and begin the play loop
         Game game = new Game();
-        game.start();
+        //game.exit();
     }
 }
